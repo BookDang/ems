b05da4a8d2bc893a66665452e062dab31ddd1a0e
DangSach <sachdang.dev@gmail.com>
2025-11-29 10:24:08 +0700

    #5 - feat: Enhance Dashboard layout and SideNav with new styles and links

--- app/(ui)/dashboard/(main)/layout.tsx
+++ app/(ui)/dashboard/(main)/layout.tsx
@@ -4,7 +4,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="dashboard-layout md:overflow-hidden login-container flex gap-2 justify-center p-3 sm:p-0 h-full w-full">
             <SideNav />
-            <div className="grow md:overflow-y-auto h-full rounded-xl bg-white/30 p-4">
+            <div className="grow md:overflow-y-auto h-full rounded-r-xl bg-white/30 p-4">
                 {children}
             </div>
         </div>
--- app/(ui)/dashboard/_components/sidenav/page.tsx
+++ app/(ui)/dashboard/_components/sidenav/page.tsx
@@ -1,31 +1,70 @@
 import Link from "next/link"
 import { FaChalkboardTeacher } from "react-icons/fa"
+import { PiStudent } from "react-icons/pi"
+import { SiGoogleclassroom } from "react-icons/si"
+import { MdOutlineSubject } from "react-icons/md"
+import { PiExam } from "react-icons/pi"
 
 const SideNav = () => {
     return (
-        <div className="side-nav h-full rounded-r-2xl bg-white max-w-64 w-full fixed top-0 left-0 hidden sm:block sm:static ">
+        <div className="side-nav h-full rounded-l-xl bg-white max-w-64 w-full fixed top-0 left-0 hidden sm:block sm:static ">
             <nav>
-                <ul className="flex flex-col p-6 space-y-4">
+                <div>
+                    <h2 className="text-2xl rounded-tl-xl font-bold text-light1-blue-ems bg-dark-blue-ems p-4 border-b border-gray-200">
+                        <Link
+                            href="#"
+                            className="hover:text-white block w-full h-full"
+                        >
+                            {/* <LuLayoutDashboard className="inline mr-2 -mt-2 text-light-orange-ems" /> */}
+                            <span>EMS Dashboard</span>
+                        </Link>
+                    </h2>
+                </div>
+                <ul className="flex flex-col p-4 space-y-6">
                     <li>
-                        <Link href="#">
-                            <FaChalkboardTeacher className="inline mr-2" />
+                        <Link
+                            href="#"
+                            className=" w-full py-2 rounded-sm hover:bg-white/25 hover:shadow-sm shadow-light-orange-ems/30 block px-4"
+                        >
+                            <FaChalkboardTeacher className="inline mr-2 text-light-orange-ems" />
                             Teachers
                         </Link>
                     </li>
                     <li>
-                        <Link href="#">Students</Link>
+                        <Link
+                            href="#"
+                            className=" w-full py-2 rounded-sm hover:bg-white/25 hover:shadow-sm shadow-light-orange-ems/30 block px-4"
+                        >
+                            <PiStudent className="inline mr-2 text-light-orange-ems" />
+                            Students
+                        </Link>
                     </li>
                     <li>
-                        <Link href="#">Classes</Link>
+                        <Link
+                            href="#"
+                            className=" w-full py-2 rounded-sm hover:bg-white/25 hover:shadow-sm shadow-light-orange-ems/30 block px-4"
+                        >
+                            <SiGoogleclassroom className="inline mr-2 text-light-orange-ems" />
+                            Classes
+                        </Link>
                     </li>
                     <li>
-                        <Link href="#">Subjects</Link>
+                        <Link
+                            href="#"
+                            className=" w-full py-2 rounded-sm hover:bg-white/25 hover:shadow-sm shadow-light-orange-ems/30 block px-4"
+                        >
+                            <MdOutlineSubject className="inline mr-2 text-light-orange-ems" />
+                            Subjects
+                        </Link>
                     </li>
                     <li>
-                        <Link href="#">Exams</Link>
-                    </li>
-                    <li>
-                        <Link href="#">Results</Link>
+                        <Link
+                            href="#"
+                            className=" w-full py-2 rounded-sm hover:bg-white/25 hover:shadow-sm shadow-light-orange-ems/30 block px-4"
+                        >
+                            <PiExam className="inline mr-2 text-light-orange-ems" />
+                            Exams
+                        </Link>
                     </li>
                 </ul>
             </nav>
