b1edf6e819a6971e8788c819a917848377f59d16
DangSach <sachdang.dev@gmail.com>
2026-01-13 16:02:33 +0700

    #10 - fix: adjust layout class for consistent flex behavior in DashboardLayout

--- nextjs-app/app/dashboard/(main)/layout.tsx
+++ nextjs-app/app/dashboard/(main)/layout.tsx
@@ -7,9 +7,9 @@ import { Provider } from "react-redux"
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <Provider store={store}>
-            <div className="dashboard-layout md:overflow-hidden login-container flex gap-2 justify-center p-3 sm:p-0 h-full w-full">
+            <div className="dashboard-layout md:overflow-hidden login-container flex gap-2 p-3 sm:p-0 h-full w-full">
                 <SideNav />
-                <div className="grow h-full rounded-r-xl bg-white/30 p-4">
+                <div className="flex-1 h-full rounded-r-xl bg-white/30 p-4">
                     {children}
                 </div>
             </div>
