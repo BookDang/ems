432055677b203cf7c4e7bc58607e97838d3a1a28
DangSach <sachdang.dev@gmail.com>
2026-01-13 13:59:56 +0700

    #10 - refactor: remove error handling logic from TeacherList and comment out simulated error in getTeachers

--- nextjs-app/app/dashboard/_components/teacher/TeacherList.tsx
+++ nextjs-app/app/dashboard/_components/teacher/TeacherList.tsx
@@ -1,21 +1,16 @@
 import React from "react"
 import TeacherTable from "@/app/dashboard/_components/teacher/TeacherTable"
 import { getTeachers } from "@/services/teacher.service"
-import Error500 from "@/app/_components/errors/Error500"
 
 export const TeacherList: React.FC = async () => {
     let teachers: unknown[] = []
-    let errorMessage = null
+    // const errorMessage = null
 
     try {
         teachers = await getTeachers()
     } catch (error: unknown) {
-        errorMessage = error instanceof Error ? error.message : String(error)
+        // errorMessage = error instanceof Error ? error.message : String(error)
         throw new Error("Something went wrong while fetching teachers.")
-    }
-
-    if (errorMessage) {
-        return <Error500 />
     }
 
     return <TeacherTable teachers={teachers} />
--- nextjs-app/services/teacher.service.ts
+++ nextjs-app/services/teacher.service.ts
@@ -2,7 +2,7 @@ export const getTeachers = async () => {
     try {
         await new Promise((resolve) => setTimeout(resolve, 3000))
         console.log("Đã chờ xong 3 giây!")
-        throw new Error("DATABASE_CONNECTION_FAILED")
+        // throw new Error("DATABASE_CONNECTION_FAILED")
         return []
     } catch (error) {
         console.error("Error fetching teachers:", error)
